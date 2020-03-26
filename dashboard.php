e7d36b49a2136f279d275f2b3232a33c191e8c7b
dhubgrob <dhubgrob@protonmail.com>
2020-03-26 10:46:45 +0100

    avec la condition (!empty($_POST) c'est mieux

--- dashboard.php
+++ dashboard.php
@@ -14,7 +14,7 @@ $dbh = new PDO
 
 // Lancement de la session
 	session_start();
-	var_dump($_SESSION['authentification']);
+	var_dump($_SESSION['userid']);
 
 	//	Si l'utilisateur n'est pas authentifié
 	if(!array_key_exists('userid', $_SESSION))
@@ -28,7 +28,7 @@ $dbh = new PDO
 // requête qui récupère le username du membre connecté
 	$query = 'SELECT username FROM writers WHERE id= :iduser';
 	$sth = $dbh->prepare($query);
-	$sth->bindValue(':iduser', trim($_SESSION['authentification']), PDO::PARAM_STR);
+	$sth->bindValue(':iduser', trim($_SESSION['userid']), PDO::PARAM_STR);
 	$sth->execute();
 	$usernameSession = $sth->fetch();
 
@@ -37,20 +37,54 @@ $dbh = new PDO
 // requête qui récupère les articles déjà publiés
 	$query = 'SELECT title, publication_date FROM posts WHERE writerid= :writerid';
 	$sth = $dbh->prepare($query);
-	$sth->bindValue(':writerid', $_SESSION['authentification'], PDO::PARAM_STR);
+	$sth->bindValue(':writerid', $_SESSION['userid'], PDO::PARAM_STR);
 	$sth->execute();
 	$publishedArticles = $sth->fetch();
 
 //Pour publication nouvel article
-
+if(!empty($_POST)) {
 	$query = 'INSERT INTO posts (title, content, writerid, image) VALUES (:title, :content, :writerid, :image)';
 	$sth = $dbh->prepare($query);
 	$sth->bindValue(':title', $_POST['title'], PDO::PARAM_STR);
 	$sth->bindValue(':content', $_POST['content'], PDO::PARAM_STR);
-	$sth->bindValue(':writerid', $_SESSION['authentification'], PDO::PARAM_STR);
+	$sth->bindValue(':writerid', $_SESSION['userid'], PDO::PARAM_STR);
 	$sth->bindValue(':image', 'null', PDO::PARAM_STR);
 	$sth->execute();
-
+}
 	var_dump($publishedArticles);
-	var_dump($_POST);
+
+//Gestion envoi de fichier
+/*
+if(array_key_exists('monFichier', $_FILES))
+	{
+		if($_FILES['monFichier']['error'] == 0)
+		{
+			if(in_array(mime_content_type($_FILES['monFichier']['tmp_name']), ['image/png', 'image/jpeg']))
+			{
+				if($_FILES['monFichier']['size'] <= 3000000)
+				{
+					move_uploaded_file($_FILES['monFichier']['tmp_name'], 'uploads/'.uniqid().'.'.pathinfo($_FILES['monFichier']['name'], PATHINFO_EXTENSION));
+
+					header('Location: ./');
+					exit;
+				}
+				else
+				{
+					echo 'Le fichier est trop volumineux…';
+				}
+			}
+			else
+			{
+				echo 'Le type mime du fichier est incorrect…';
+			}
+		}
+		else
+		{
+			echo 'Le fichier n\'a pas pu être récupéré…';
+		}
+	}
+*/
+
+
+
 	include 'dashboard.phtml';
